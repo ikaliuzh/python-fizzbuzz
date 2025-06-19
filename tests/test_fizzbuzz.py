9bc520460cf2d7f71a0b575fcebfe237c26e6697
ikaliuzh (Illia Kaliuzhnyi) <ikaliuzh@cisco.com>
2025-06-19 13:50:31 +0300

    Refactor fizzbuzz functions and add additional implementations with corresponding tests

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -1,5 +1,10 @@
 from app.fizzbuzz import fizzbuzz
 
+from app.fizzbuzz import (
+    fizzbuzz1,
+    fizzbuzz2,
+)
+
 def test_works_with_1():
     # Arrange
     num = 1
@@ -91,3 +96,121 @@ def test_works_with_15():
         assert answer[index] == expected_answer[index]
 
 
+def test_fizzbuzz1_works_with_1():
+    # Arrange
+    num = 1
+    expected_answer = ["1"]
+
+    # Act
+    answer = fizzbuzz1(num)
+
+    # Assert
+    assert len(answer) == len(expected_answer)
+
+    for index in range(0, len(expected_answer)):
+        assert answer[index] == expected_answer[index]
+
+
+def test_fizzbuzz1_works_with_3():
+    # Arrange
+    num = 3
+    expected_answer = ["1", "2", "Fizz"]
+
+    # Act
+    answer = fizzbuzz1(num)
+
+    # Assert
+    assert len(answer) == len(expected_answer)
+
+    for index in range(0, len(expected_answer)):
+        assert answer[index] == expected_answer[index]
+
+
+def test_fizzbuzz1_works_with_5():
+    # Arrange
+    num = 5
+    expected_answer = ["1", "2", "Fizz", "4", "Buzz"]
+
+    # Act
+    answer = fizzbuzz1(num)
+
+    # Assert
+    assert len(answer) == len(expected_answer)
+
+    for index in range(0, len(expected_answer)):
+        assert answer[index] == expected_answer[index]
+
+
+def test_fizzbuzz1_works_with_15():
+    # Arrange
+    num = 15
+    expected_answer = ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "Fizz"]
+
+    # Act
+    answer = fizzbuzz1(num)
+
+    # Assert
+    assert len(answer) == len(expected_answer)
+
+    for index in range(0, len(expected_answer)):
+        assert answer[index] == expected_answer[index]
+
+
+def test_fizzbuzz2_works_with_1():
+    # Arrange
+    num = 1
+    expected_answer = ["1"]
+
+    # Act
+    answer = fizzbuzz2(num)
+
+    # Assert
+    assert len(answer) == len(expected_answer)
+
+    for index in range(0, len(expected_answer)):
+        assert answer[index] == expected_answer[index]
+
+
+def test_fizzbuzz2_works_with_3():
+    # Arrange
+    num = 3
+    expected_answer = ["1", "2", "Fizz"]
+
+    # Act
+    answer = fizzbuzz2(num)
+
+    # Assert
+    assert len(answer) == len(expected_answer)
+
+    for index in range(0, len(expected_answer)):
+        assert answer[index] == expected_answer[index]
+
+
+def test_fizzbuzz2_works_with_5():
+    # Arrange
+    num = 5
+    expected_answer = ["1", "2", "Fizz", "4", "Buzz"]
+
+    # Act
+    answer = fizzbuzz2(num)
+
+    # Assert
+    assert len(answer) == len(expected_answer)
+
+    for index in range(0, len(expected_answer)):
+        assert answer[index] == expected_answer[index]
+
+
+def test_fizzbuzz2_works_with_15():
+    # Arrange
+    num = 15
+    expected_answer = ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "Fizz"]
+
+    # Act
+    answer = fizzbuzz2(num)
+
+    # Assert
+    assert len(answer) == len(expected_answer)
+
+    for index in range(0, len(expected_answer)):
+        assert answer[index] == expected_answer[index]
