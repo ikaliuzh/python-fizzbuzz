937a0c5551231bba504c58c4e0573467e4d1b9d2
ikaliuzh (Illia Kaliuzhnyi) <ikaliuzh@cisco.com>
2025-06-19 13:54:12 +0300

    Add FizzBuzz class implementation with corresponding tests

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -5,6 +5,8 @@ from app.fizzbuzz import (
     fizzbuzz2,
 )
 
+from app.fizzbuzz import FizzBuzz
+
 def test_works_with_1():
     # Arrange
     num = 1
@@ -214,3 +216,68 @@ def test_fizzbuzz2_works_with_15():
 
     for index in range(0, len(expected_answer)):
         assert answer[index] == expected_answer[index]
+
+
+def test_fizzbuzz_class_works_with_1():
+    # Arrange
+    fb = FizzBuzz()
+    num = 1
+    expected_answer = ["1"]
+
+    # Act
+    answer = fb.fizzbuzz(num)
+
+    # Assert
+    assert len(answer) == len(expected_answer)
+
+    for index in range(0, len(expected_answer)):
+        assert answer[index] == expected_answer[index]
+
+
+def test_fizzbuzz_class_works_with_3():
+    # Arrange
+    fb = FizzBuzz()
+    num = 3
+    expected_answer = ["1", "2", "Fizz"]
+
+    # Act
+    answer = fb.fizzbuzz(num)
+
+    # Assert
+    assert len(answer) == len(expected_answer)
+
+    for index in range(0, len(expected_answer)):
+        assert answer[index] == expected_answer[index]
+
+
+def test_fizzbuzz_class_works_with_5():
+    # Arrange
+    fb = FizzBuzz()
+    num = 5
+    expected_answer = ["1", "2", "Fizz", "4", "Buzz"]
+
+    # Act
+    answer = fb.fizzbuzz(num)
+
+    # Assert
+    assert len(answer) == len(expected_answer)
+
+    for index in range(0, len(expected_answer)):
+        assert answer[index] == expected_answer[index]
+
+
+def test_fizzbuzz_class_works_with_15():
+    # Arrange
+    fb = FizzBuzz()
+    num = 15
+    expected_answer = ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"]
+
+    # Act
+    answer = fb.fizzbuzz(num)
+
+    # Assert
+    assert len(answer) == len(expected_answer)
+
+    for index in range(0, len(expected_answer)):
+        assert answer[index] == expected_answer[index]
+
